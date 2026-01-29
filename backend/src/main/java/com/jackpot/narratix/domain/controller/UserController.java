d8015777bb4e7407ab45f5e345687e7401749f5e
livebylee <livebylee@gmail.com>
2026-01-29 18:16:42 +0900

    [refactor] 로그인과 회원가입 시 토큰 발급 및 반환 로직 공통화  (#221)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/UserController.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/UserController.java
@@ -4,10 +4,8 @@ import com.jackpot.narratix.domain.controller.dto.JoinRequest;
 import com.jackpot.narratix.domain.controller.dto.LoginRequest;
 import com.jackpot.narratix.domain.controller.dto.UserTokenResponse;
 import com.jackpot.narratix.domain.service.UserService;
-import com.jackpot.narratix.global.auth.jwt.service.JwtGenerator;
 import com.jackpot.narratix.global.auth.jwt.service.TokenService;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.mapping.Join;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +17,6 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
-    private final JwtGenerator jwtGenerator;
     private final TokenService tokenService;
 
     @PostMapping("/auth")
@@ -33,40 +30,31 @@ public class UserController {
         if (!request.getPassword().equals(request.getPasswordConfirm())) {
             throw new IllegalArgumentException("비밀번호가 일치하지 않습니다");
         }
-
-        userService.join(request.getUserId(), request.getPassword(), request.getNickname());
-
-        var tokens = tokenService.issueToken(request.getUserId());
-
-        String accessTokenValue = tokens.getAccessToken();
-        String refreshTokenValue = tokens.getRefreshToken();
-
-        ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshTokenValue)
-                .httpOnly(true)
-                .build();
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, cookie.toString())
-                .body(new UserTokenResponse(accessTokenValue));
+        return createTokenResponse(request.getUserId());
     }
 
     @PostMapping("/auth/login")
     public ResponseEntity<UserTokenResponse> login(@RequestBody LoginRequest request) {
+
         userService.login(request);
 
-        var tokens = tokenService.issueToken(request.getUserId());
+        return createTokenResponse(request.getUserId());
+    }
 
+    private ResponseEntity<UserTokenResponse> createTokenResponse(String userId) {
+
+        var tokens = tokenService.issueToken(userId);
         String accessTokenValue = tokens.getAccessToken();
         String refreshTokenValue = tokens.getRefreshToken();
 
         ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshTokenValue)
                 .httpOnly(true)
+                .path("/")
                 .build();
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
                 .body(new UserTokenResponse(accessTokenValue));
-
     }
 }
 
