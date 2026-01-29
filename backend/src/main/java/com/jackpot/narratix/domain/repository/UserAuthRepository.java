97c6b40f96c39b8eedb595359f0663aae91fe8cb
livebylee <livebylee@gmail.com>
2026-01-29 12:03:18 +0900

    [feat] user join 서비스 구현(#221)

--- backend/src/main/java/com/jackpot/narratix/domain/repository/UserAuthRepository.java
+++ backend/src/main/java/com/jackpot/narratix/domain/repository/UserAuthRepository.java
@@ -1,8 +1,9 @@
 package com.jackpot.narratix.domain.repository;
 
 import com.jackpot.narratix.domain.entity.User;
+import com.jackpot.narratix.domain.entity.UserAuth;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserAuthRepository extends JpaRepository<User, String> {
+public interface UserAuthRepository extends JpaRepository<UserAuth, String> {
 
 }
--- backend/src/main/java/com/jackpot/narratix/domain/service/UserService.java
+++ backend/src/main/java/com/jackpot/narratix/domain/service/UserService.java
@@ -1,6 +1,8 @@
 package com.jackpot.narratix.domain.service;
 
 import com.jackpot.narratix.domain.entity.User;
+import com.jackpot.narratix.domain.entity.UserAuth;
+import com.jackpot.narratix.domain.repository.UserAuthRepository;
 import com.jackpot.narratix.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,13 +14,28 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserAuthRepository userAuthRepository;
 
     public boolean isIdDuplicated(String id) {
         return userRepository.existsById(id);
     }
 
     @Transactional
-    public void join(User user) {
+    public void join(String id, String password, String nickname) {
+        if (isIdDuplicated(id)) {
+            throw new IllegalArgumentException("이미 존재하는 아이디");
+        }
+
+        User user = User.builder()
+                .id(id)
+                .nickname(nickname)
+                .build();
         userRepository.save(user);
+
+        UserAuth auth = UserAuth.builder()
+                .userId(id)
+                .password(password)
+                .build();
+        userAuthRepository.save(auth);
     }
 }
