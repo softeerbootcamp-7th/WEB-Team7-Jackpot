6b8e583db59edd621fd8497693b3e305e83ca850
livebylee <livebylee@gmail.com>
2026-01-29 11:31:25 +0900

    [feat] UserService transactional 추가 (#221)

--- backend/src/main/java/com/jackpot/narratix/domain/service/UserService.java
+++ backend/src/main/java/com/jackpot/narratix/domain/service/UserService.java
@@ -4,18 +4,21 @@ import com.jackpot.narratix.domain.entity.User;
 import com.jackpot.narratix.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
     private final UserRepository userRepository;
 
-    public boolean isIdDuplicated(String id){
+    public boolean isIdDuplicated(String id) {
         return userRepository.existsById(id);
     }
 
-    public void join(User user){
+    @Transactional
+    public void join(User user) {
         userRepository.save(user);
     }
 }
