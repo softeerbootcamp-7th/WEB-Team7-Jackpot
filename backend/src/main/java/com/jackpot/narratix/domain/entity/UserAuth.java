f4801f27c34c2febd51ec4041512a3c79cb0fa1d
livebylee <livebylee@gmail.com>
2026-01-29 11:55:25 +0900

    [feat] UserAuth에 빌더 패턴 추가 (#221)

--- backend/src/main/java/com/jackpot/narratix/domain/entity/UserAuth.java
+++ backend/src/main/java/com/jackpot/narratix/domain/entity/UserAuth.java
@@ -3,6 +3,7 @@ package com.jackpot.narratix.domain.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -15,4 +16,10 @@ public class UserAuth {
     private String userId;
 
     private String password;
+
+    @Builder
+    public UserAuth(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
 }
