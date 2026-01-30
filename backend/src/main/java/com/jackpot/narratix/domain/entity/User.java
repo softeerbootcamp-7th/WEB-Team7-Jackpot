2c9eb715e7b18328005feb49d114dbf3080342db
kseysh <kseysh123@naver.com>
2026-01-30 17:24:14 +0900

    [feat] User 및 UserAuth 엔티티에 DDL 제약조건 추 (#261)

--- backend/src/main/java/com/jackpot/narratix/domain/entity/User.java
+++ backend/src/main/java/com/jackpot/narratix/domain/entity/User.java
@@ -1,12 +1,16 @@
 package com.jackpot.narratix.domain.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseTimeEntity {
@@ -14,5 +18,7 @@ public class User extends BaseTimeEntity {
     @Id
     private String id;
 
+    @NotNull
+    @Column(name = "nickname", nullable = false)
     private String nickname;
 }
--- backend/src/main/java/com/jackpot/narratix/domain/entity/UserAuth.java
+++ backend/src/main/java/com/jackpot/narratix/domain/entity/UserAuth.java
@@ -1,18 +1,23 @@
 package com.jackpot.narratix.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@Table(name = "user_auth")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserAuth extends BaseTimeEntity {
 
     @Id
-    private String userId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 
+    @NotNull
+    @Column(name = "password", nullable = false)
     private String password;
 }
