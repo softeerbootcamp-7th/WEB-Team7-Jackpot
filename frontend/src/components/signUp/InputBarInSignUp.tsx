38e9fa0a06165a25dc8dc2deb2fb510bf17120ca
윤종근 <doodukly1999@gmail.com>
2026-02-01 13:11:11 +0900

    [design] 유효성 검증 여부에 따른 디자인 분기 처리

--- frontend/src/components/signUp/InputBarInSignUp.tsx
+++ frontend/src/components/signUp/InputBarInSignUp.tsx
@@ -3,19 +3,41 @@ import InputBar from '@/components/common/InputBar';
 
 interface InputBarInSignUpProps extends InputBarProps {
   hintText: string;
+  helpMessage?: string;
+  isSuccess?: boolean;
 }
 
 const InputBarInSignUp = ({
   hintText,
+  helpMessage,
+  isSuccess = false,
   ...inputBarProps
 }: InputBarInSignUpProps) => {
+  const isError = helpMessage && !isSuccess;
+
   return (
     <div className='flex flex-col w-full gap-3'>
       <div className='flex gap-[0.125rem]'>
         <div className='text-gray-950 font-bold text-lg'>{hintText}</div>
         <div className='text-red-600'>*</div>
       </div>
-      <InputBar {...inputBarProps} />
+
+      <InputBar
+        className={`border ${
+          isError ? 'border-red-500' : 'border-transparent'
+        }`}
+        {...inputBarProps}
+      />
+
+      {helpMessage && (
+        <p
+          className={`text-xs mt-1 ps-1 ${
+            isSuccess ? 'text-blue-500' : 'text-red-500'
+          }`}
+        >
+          {helpMessage}
+        </p>
+      )}
     </div>
   );
 };
