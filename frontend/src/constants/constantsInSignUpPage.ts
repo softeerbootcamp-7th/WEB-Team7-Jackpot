111486ec7def248de79b663f872dc1530f5847c4
윤종근 <doodukly1999@gmail.com>
2026-02-01 13:03:48 +0900

    [feat] 유효성 검사를 위한 각 입력바의 최대 길이 상수 추가

--- frontend/src/constants/constantsInSignUpPage.ts
+++ frontend/src/constants/constantsInSignUpPage.ts
@@ -4,24 +4,28 @@ export const INPUT_BAR_IN_SIGNUP = [
     HINT_TEXT: '아이디',
     TYPE: 'text',
     PLACEHOLDER: '아이디를 입력해주세요',
+    MAX_LENGTH: 12,
   },
   {
     ID: 'password',
     HINT_TEXT: '비밀번호',
     TYPE: 'password',
     PLACEHOLDER: '영문 숫자 조합 8자 이상으로 설정해주세요',
+    MAX_LENGTH: 20,
   },
   {
     ID: 'passwordCheck',
     HINT_TEXT: '비밀번호 확인',
     TYPE: 'password',
     PLACEHOLDER: '설정하신 비밀번호를 입력해주세요',
+    MAX_LENGTH: 20,
   },
   {
     ID: 'nickname',
     HINT_TEXT: '사용자 이름',
     TYPE: 'text',
     PLACEHOLDER: '네러틱스에서 사용하고 싶은 이름을 설정해주세요',
+    MAX_LENGTH: 15,
   },
 ] as const;
 
