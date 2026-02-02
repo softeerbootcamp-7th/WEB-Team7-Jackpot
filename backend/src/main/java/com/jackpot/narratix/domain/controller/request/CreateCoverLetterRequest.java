2f4595eea9725b2b01cb7b9b7035aa5fbd4cc114
kseysh <kseysh123@naver.com>
2026-02-02 17:53:38 +0900

    [chore] 코드 정 (#236)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/request/CreateCoverLetterRequest.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/request/CreateCoverLetterRequest.java
@@ -1,6 +1,7 @@
 package com.jackpot.narratix.domain.controller.request;
 
 import com.jackpot.narratix.domain.entity.enums.ApplyHalfType;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -11,8 +12,10 @@ public record CreateCoverLetterRequest(
         @NotNull(message = "기업명은 필수 입력 항목입니다.") String companyName,
         @NotNull(message = "채용년도는 필수 입력 항목입니다.") Integer applyYear,
         @NotNull(message = "채용분기는 필수 입력 항목입니다.") ApplyHalfType applyHalf,
-        @NotNull(message = "직무명은 필수 입력 항목입니다.") String jobPosition, LocalDate deadline,
+        @NotNull(message = "직무명은 필수 입력 항목입니다.") String jobPosition,
+        @Nullable LocalDate deadline,
         @NotNull(message = "질문은 필수 입력 항목입니다.")
         @Size(min = 1, max = 3, message = "질문은 최소 1개에서 최대 3개까지 입력할 수 있습니다.")
-        List<CreateQuestionRequest> questions) {
+        List<CreateQuestionRequest> questions
+) {
 }
--- backend/src/main/java/com/jackpot/narratix/domain/controller/request/CreateQuestionRequest.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/request/CreateQuestionRequest.java
@@ -3,7 +3,10 @@ package com.jackpot.narratix.domain.controller.request;
 import com.jackpot.narratix.domain.entity.enums.QuestionCategoryType;
 import jakarta.validation.constraints.NotNull;
 
-public record CreateQuestionRequest(@NotNull String question, @NotNull QuestionCategoryType category) {
+public record CreateQuestionRequest(
+        @NotNull(message = "질문은 필수 입력 항목입니다.") String question,
+        @NotNull(message = "문항 유형은 필수 입력 항목입니다.") QuestionCategoryType category
+) {
 
     public CreateQuestionRequest(String question, String category) {
         this(question, QuestionCategoryType.fromDescription(category));
