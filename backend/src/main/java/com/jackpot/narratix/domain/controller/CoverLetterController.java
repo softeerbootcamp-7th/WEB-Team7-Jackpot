d5d4b6cca5b64f5b2e5366b954c2bf0bf44d6474
kseysh <kseysh123@naver.com>
2026-02-02 14:35:41 +0900

    [feat] 자기소개서 등록 controller 로직 구현 (#236)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/CoverLetterController.java
@@ -0,0 +1,29 @@
+package com.jackpot.narratix.domain.controller;
+
+import com.jackpot.narratix.domain.controller.request.CreateCoverLetterRequest;
+import com.jackpot.narratix.domain.controller.response.CreateCoverLetterResponse;
+import com.jackpot.narratix.domain.service.CoverLetterService;
+import com.jackpot.narratix.global.auth.UserId;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/coverletter")
+public class CoverLetterController {
+
+    private final CoverLetterService coverLetterService;
+
+    @PostMapping
+    public ResponseEntity<CreateCoverLetterResponse> createCoverLetter(
+            @UserId String userId,
+            @RequestBody @Valid CreateCoverLetterRequest createCoverLetterRequest
+    ) {
+        return ResponseEntity.ok(coverLetterService.createNewCoverLetter(userId, createCoverLetterRequest));
+    }
+}
