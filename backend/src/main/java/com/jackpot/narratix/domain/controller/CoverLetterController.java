44085e0bf22ae54e5dca6eeb986a5f4eeef7ac96
kseysh <kseysh123@naver.com>
2026-02-03 10:42:08 +0900

    [feat] 자기소개서 합계 조회 컨트롤러 로직 추 (#238)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/CoverLetterController.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/CoverLetterController.java
@@ -2,15 +2,16 @@ package com.jackpot.narratix.domain.controller;
 
 import com.jackpot.narratix.domain.controller.request.CreateCoverLetterRequest;
 import com.jackpot.narratix.domain.controller.response.CreateCoverLetterResponse;
+import com.jackpot.narratix.domain.controller.response.TotalCoverLetterCountResponse;
 import com.jackpot.narratix.domain.service.CoverLetterService;
 import com.jackpot.narratix.global.auth.UserId;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
 
 @RestController
 @RequiredArgsConstructor
@@ -25,5 +26,13 @@ public class CoverLetterController {
             @RequestBody @Valid CreateCoverLetterRequest createCoverLetterRequest
     ) {
         return ResponseEntity.ok(coverLetterService.createNewCoverLetter(userId, createCoverLetterRequest));
+    }
+
+    @GetMapping("/count")
+    public ResponseEntity<TotalCoverLetterCountResponse> getTotalCoverLetterCount(
+            @UserId String userId,
+            @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date
+    ) {
+        return ResponseEntity.ok(coverLetterService.getTotalCoverLetterCount(userId, date));
     }
 }
