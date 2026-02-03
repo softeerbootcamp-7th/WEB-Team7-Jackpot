62eecf35d8e3e255b23df82cc4e66e51a1d2126e
윤종근 <doodukly1999@gmail.com>
2026-02-03 20:26:55 +0900

    [feat] 단계에 따른 업로드 콘텐츠 영역 조건부 렌더링

--- frontend/src/components/upload/ContentArea.tsx
+++ frontend/src/components/upload/ContentArea.tsx
@@ -14,8 +14,18 @@ const ContentArea = ({
 }: FirstContentAreaHeaderProps) => {
   return (
     <div className='flex flex-col gap-4 p-6 border border-gray-100 rounded-2xl'>
-      <ContentAreaHeader uploadTab={uploadTab} setUploadTab={setUploadTab} />
-      {uploadTab === 'file' ? <UploadFileArea /> : <UploadTextArea />}
+      {step === '1' && (
+        <>
+          <FirstContentAreaHeader
+            uploadTab={uploadTab}
+            setUploadTab={setUploadTab}
+            nextStep={nextStep}
+          />
+          {uploadTab === 'file' ? <UploadFileArea /> : <UploadTextArea />}
+        </>
+      )}
+      {step === '2' && <SecondContentAreaHeader />}
+      {step === '3' && <>3</>}
     </div>
   );
 };
