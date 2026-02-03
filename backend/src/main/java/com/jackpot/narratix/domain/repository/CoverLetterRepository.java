c9c5603cecb2fd5b79459fe7fdd88935f14211cf
kseysh <kseysh123@naver.com>
2026-02-03 11:03:50 +0900

    [chore] 불필요한 어노테이션 및 함수 제거 (#270)

--- backend/src/main/java/com/jackpot/narratix/domain/repository/CoverLetterRepository.java
+++ backend/src/main/java/com/jackpot/narratix/domain/repository/CoverLetterRepository.java
@@ -1,11 +1,9 @@
 package com.jackpot.narratix.domain.repository;
 
 import com.jackpot.narratix.domain.entity.CoverLetter;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface CoverLetterRepository {
 
     CoverLetter save(CoverLetter coverLetter);
--- backend/src/main/java/com/jackpot/narratix/domain/repository/UserRepository.java
+++ backend/src/main/java/com/jackpot/narratix/domain/repository/UserRepository.java
@@ -1,12 +1,8 @@
 package com.jackpot.narratix.domain.repository;
 
 import com.jackpot.narratix.domain.entity.User;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository {
 
     User getReferenceById(String userId);
-
-    boolean existsById(String userId);
 }
