7687f277c698e5278092ae877cc296457585debb
kseysh <kseysh123@naver.com>
2026-02-03 19:02:35 +0900

    [chore] 불필요한 import 제거 (#278)

--- backend/src/main/java/com/jackpot/narratix/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/jackpot/narratix/global/exception/GlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package com.jackpot.narratix.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConversionException;
-import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
