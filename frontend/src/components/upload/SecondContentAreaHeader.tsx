a92d110535e42dcc3238c5cdcc07eefebfd66dd6
윤종근 <doodukly1999@gmail.com>
2026-02-04 23:07:29 +0900

    [feat] 자료 업로드 페이지 두 번째 단계의 콘텐츠 영역 헤더 구현

---
+++ frontend/src/components/upload/SecondContentAreaHeader.tsx
@@ -0,0 +1,23 @@
+import { UploadPageIcons } from '@/components/upload/icons';
+
+const SecondContentAreaHeader = () => {
+  return (
+    <div>
+      <div className='flex justify-between items-center select-none'>
+        <div className='flex gap-[0.625rem]'>
+          <UploadPageIcons.AILabelingIcon size='32' />
+          <div className='font-bold text-2xl'>
+            <span className='text-purple-500'>두 번째</span> 자기소개서는 총
+            <span className='text-purple-500'> 3개</span>의 문항으로
+            분류되었어요!
+          </div>
+        </div>
+        <button className='px-5 py-3 rounded-lg bg-gray-900 text-white font-bold text-lg cursor-pointer'>
+          저장하기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SecondContentAreaHeader;
