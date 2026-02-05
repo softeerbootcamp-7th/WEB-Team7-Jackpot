41587ef45cf28c21b0e10e2c38c87993daec889e
윤종근 <doodukly1999@gmail.com>
2026-02-05 16:18:26 +0900

    [design] 사용자 아바타 아이콘 추가

--- frontend/src/components/common/icons/index.ts
+++ frontend/src/components/common/icons/index.ts
@@ -1,5 +1,7 @@
 import NotificationIcon from '@/components/common/icons/NotificationIcon';
+import UserAvatarIcon from '@/components/common/icons/UserAvatarIcon';
 
 export const CommonIcon = {
   NotificationIcon: NotificationIcon,
+  UserAvatarIcon: UserAvatarIcon,
 };
