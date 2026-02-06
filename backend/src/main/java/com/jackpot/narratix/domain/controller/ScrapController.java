e88d177e97d2a79b7f1ddc277172ac9f4b83f170
livebylee <livebylee@gmail.com>
2026-02-06 19:51:16 +0900

    [feat] controller 구현 (#234)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/ScrapController.java
@@ -0,0 +1,26 @@
+package com.jackpot.narratix.domain.controller;
+
+import com.jackpot.narratix.domain.controller.response.CreateScrapResponse;
+import com.jackpot.narratix.domain.service.ScrapService;
+import com.jackpot.narratix.global.auth.UserId;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/scrap")
+public class ScrapController {
+
+    @PostMapping
+    public ResponseEntity<CreateScrapResponse> createCoverLetter(
+            @UserId String userId,
+            @RequestBody @Valid Long questionId
+    ) {
+        return ResponseEntity.ok(ScrapService.createScrap(userId, questionId));
+    }
+}
