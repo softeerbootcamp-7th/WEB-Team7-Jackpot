58d50031a4f2d7dce0b0a520c322d4022e0b3548
윤종근 <doodukly1999@gmail.com>
2026-02-06 09:37:00 +0900

    [style] lint 미적용 부분 자동 적용 처리

--- frontend/src/components/auth/SubmitButton.tsx
+++ frontend/src/components/auth/SubmitButton.tsx
@@ -3,14 +3,14 @@ interface SubmitButtonProps {
   value: string;
 }
 
-const SubmitButton = ({isActived, value}: SubmitButtonProps) => {
-    const buttonActiveStyle: string = isActived
+const SubmitButton = ({ isActived, value }: SubmitButtonProps) => {
+  const buttonActiveStyle: string = isActived
     ? 'bg-gray-900 text-white cursor-pointer'
     : 'bg-gray-50 text-gray-400';
 
   return (
     <input
-      className={`w-full ${buttonActiveStyle} px-5 py-[0.75rem] rounded-lg`}
+      className={`w-full ${buttonActiveStyle} rounded-lg px-5 py-[0.75rem]`}
       type='submit'
       disabled={!isActived}
       value={value}
