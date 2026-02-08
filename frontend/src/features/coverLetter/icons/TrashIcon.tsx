92b263e987973fdca3fdcd525be70d51490f7855
using6843@gmail.com <using6843@gmail.com>
2026-02-08 19:15:22 +0900

    [feat] 아이콘 컴포넌트 공통화 및 구조 정리
    - 리뷰 전용 아이콘을 shared/icons로 이동
    - 커버레터 전용 아이콘 신규 추가
    - 아이콘 export 구조 정리

---
+++ frontend/src/features/coverLetter/icons/TrashIcon.tsx
@@ -0,0 +1,56 @@
+const TrashIcon = () => {
+  return (
+    <svg
+      width='16'
+      height='16'
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <g clipPath='url(#clip0_172_2493)'>
+        <path
+          d='M6.66699 7.33337V11.3334'
+          stroke='#ED2015'
+          strokeWidth='1.33333'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+        <path
+          d='M9.33301 7.33337V11.3334'
+          stroke='#ED2015'
+          strokeWidth='1.33333'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+        <path
+          d='M12.6663 4V13.3333C12.6663 13.687 12.5259 14.0261 12.2758 14.2761C12.0258 14.5262 11.6866 14.6667 11.333 14.6667H4.66634C4.31272 14.6667 3.97358 14.5262 3.72353 14.2761C3.47348 14.0261 3.33301 13.687 3.33301 13.3333V4'
+          stroke='#ED2015'
+          strokeWidth='1.33333'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+        <path
+          d='M2 4H14'
+          stroke='#ED2015'
+          strokeWidth='1.33333'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+        <path
+          d='M5.33301 4.00004V2.66671C5.33301 2.31309 5.47348 1.97395 5.72353 1.7239C5.97358 1.47385 6.31272 1.33337 6.66634 1.33337H9.33301C9.68663 1.33337 10.0258 1.47385 10.2758 1.7239C10.5259 1.97395 10.6663 2.31309 10.6663 2.66671V4.00004'
+          stroke='#ED2015'
+          strokeWidth='1.33333'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      </g>
+      <defs>
+        <clipPath id='clip0_172_2493'>
+          <rect width='16' height='16' fill='white' />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default TrashIcon;
