1a5a3b79906a719a0903db74928ee9ff9eacbcd4
Sminp <0102262@gmail.com>
2026-02-08 19:56:33 +0900

    [feat] 자기소개서 작성 엠티 케이스 추가

--- frontend/src/pages/CoverLetterLandingPage.tsx
+++ frontend/src/pages/CoverLetterLandingPage.tsx
@@ -1,12 +1,18 @@
 import NewCoverLetterButton from '@/features/coverLetter/components/NewCoverLetterButton';
+import { emptyCaseText } from '@/features/coverLetter/constants';
 import CoverLetterOverview from '@/shared/components/CoverLetterOverview';
+import DataGuard from '@/shared/components/DataGuard';
+import EmptyCase from '@/shared/components/EmptyCase';
 import SearchInput from '@/shared/components/SearchInput';
 
 const CoverLetterLandingPage = () => {
+  const hasData = true;
   const handleSearch = () => {};
 
+  const coverLetterEmptyCaseText = emptyCaseText['overview'];
+
   return (
-    <div className='flex h-screen w-full max-w-screen min-w-[1700px] flex-col overflow-hidden px-75 pb-30'>
+    <div className='flex h-[calc(100vh-5.625rem)] w-full max-w-screen min-w-[1700px] flex-col overflow-hidden px-75 pb-30'>
       <div className='flex flex-row items-center justify-between'>
         <SearchInput
           onSearch={handleSearch}
@@ -15,7 +21,12 @@ const CoverLetterLandingPage = () => {
         {/* [박소민] TODO: Link로 변환 */}
         <NewCoverLetterButton />
       </div>
-      <CoverLetterOverview isCoverLetter={true} len={9} />
+      <DataGuard
+        data={hasData}
+        fallback={<EmptyCase {...coverLetterEmptyCaseText} />}
+      >
+        <CoverLetterOverview isCoverLetter={true} len={9} />
+      </DataGuard>
     </div>
   );
 };
