879e9835a7783d422e234087e332dea81bb4f616
윤종근 <doodukly1999@gmail.com>
2026-02-08 10:25:44 +0900

    [design] 아이콘 잘림 현상으로 정적 파일로 불러오는 방식으로 수정

--- frontend/src/shared/components/ToastMessage.tsx
+++ frontend/src/shared/components/ToastMessage.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 
-import { CommonIcon as I } from '@/shared/icons';
+import doneIcon from '/images/doneIcon.png';
+import failIcon from '/images/failIcon.png';
 
 interface ToastProps {
   message: string;
@@ -19,9 +20,9 @@ const ToastMessage = ({ message, onClose, duration = 3000, status }: ToastProps)
     <div className='fixed right-0 bottom-0 left-0 z-50 flex justify-center pb-4'>
       <div className='animate-fade-in-out z-50 flex items-center gap-4 rounded-lg bg-black/60 px-6 py-3 text-sm font-semibold text-white shadow-lg backdrop-blur-md'>
         {status ? (
-          <I.DoneIcon width='20' height='20' />
+          <img src={doneIcon} alt='done' className='h-5 w-5' />
         ) : (
-          <I.FailIcon width='20' height='20' />
+          <img src={failIcon} alt='fail' className='h-5 w-5' />
         )}
         <span>{message}</span>
       </div>
--- frontend/src/shared/icons/index.ts
+++ frontend/src/shared/icons/index.ts
@@ -1,5 +1,3 @@
-import DoneIcon from '@/shared/icons/DoneIcon';
-import FailIcon from '@/shared/icons/FailIcon';
 import NotificationIcon from '@/shared/icons/NotificationIcon';
 import TitleLogo from '@/shared/icons/TitleLogo';
 import UserAvatarIcon from '@/shared/icons/UserAvatarIcon';
@@ -8,6 +6,4 @@ export const CommonIcon = {
   NotificationIcon: NotificationIcon,
   UserAvatarIcon: UserAvatarIcon,
   TitleLogo: TitleLogo,
-  DoneIcon: DoneIcon,
-  FailIcon: FailIcon,
 };
