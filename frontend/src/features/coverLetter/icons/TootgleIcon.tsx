9d635ede6f93032f7c80e925e092600000847b84
using6843@gmail.com <using6843@gmail.com>
2026-02-08 19:15:22 +0900

    [feat] 아이콘 컴포넌트 공통화 및 구조 정리
    - 리뷰 전용 아이콘을 shared/icons로 이동
    - 커버레터 전용 아이콘 신규 추가
    - 아이콘 export 구조 정리

---
+++ frontend/src/features/coverLetter/icons/TootgleIcon.tsx
@@ -0,0 +1,28 @@
+const ToogleIcon = () => {
+  return (
+    <svg
+      width='16'
+      height='16'
+      viewBox='0 0 16 16'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <path
+        d='M10 10C11.1046 10 12 9.10457 12 8C12 6.89543 11.1046 6 10 6C8.89543 6 8 6.89543 8 8C8 9.10457 8.89543 10 10 10Z'
+        stroke='#3D3D3D'
+        strokeWidth='1.4'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+      <path
+        d='M9.99967 3.33337H5.99967C3.42235 3.33337 1.33301 5.42271 1.33301 8.00004C1.33301 10.5774 3.42235 12.6667 5.99967 12.6667H9.99967C12.577 12.6667 14.6663 10.5774 14.6663 8.00004C14.6663 5.42271 12.577 3.33337 9.99967 3.33337Z'
+        stroke='#3D3D3D'
+        strokeWidth='1.4'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      />
+    </svg>
+  );
+};
+
+export default ToogleIcon;
