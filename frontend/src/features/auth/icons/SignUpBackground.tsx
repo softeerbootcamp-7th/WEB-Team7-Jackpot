35ff04140af22296dc18f4162f06d8b2da83af46
윤종근 <doodukly1999@gmail.com>
2026-02-08 22:25:19 +0900

    [design] 회원가입 완료 페이지에서 사용될 아이콘 추가

---
+++ frontend/src/features/auth/icons/SignUpBackground.tsx
@@ -0,0 +1,85 @@
+const SignUpBackground = () => {
+  return (
+    <svg
+      className='overflow-visible'
+      width='900'
+      height='900'
+      viewBox='0 0 900 900'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+    >
+      <circle
+        className='animate-spread'
+        style={{ animationDelay: '1s' }}
+        cx='449.862'
+        cy='449.862'
+        r='449.862'
+        fill='url(#paint0_linear_12621_2183)'
+        fillOpacity='0.9'
+      />
+
+      <circle
+        className='animate-spread'
+        style={{ animationDelay: '0.5s' }}
+        cx='449.851'
+        cy='449.861'
+        r='322.03'
+        fill='url(#paint1_linear_12621_2183)'
+        fillOpacity='0.9'
+      />
+
+      <circle
+        className='animate-spread'
+        style={{ animationDelay: '0s' }}
+        cx='449.864'
+        cy='449.863'
+        r='200.868'
+        fill='url(#paint2_linear_12621_2183)'
+        fillOpacity='0.9'
+      />
+      <defs>
+        <linearGradient
+          id='paint0_linear_12621_2183'
+          x1='449.862'
+          y1='-3.05176e-05'
+          x2='449.862'
+          y2='899.725'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#DBEBFF' />
+          <stop offset='0.234014' stopColor='#F0F2FD' stopOpacity='0.05' />
+          <stop offset='0.771712' stopColor='#F0F2FD' stopOpacity='0.05' />
+          <stop offset='1' stopColor='#EDF6FF' />
+        </linearGradient>
+        <linearGradient
+          id='paint1_linear_12621_2183'
+          x1='449.851'
+          y1='127.831'
+          x2='449.851'
+          y2='771.891'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#DBEBFF' />
+          <stop offset='0.234014' stopColor='#F0F2FD' stopOpacity='0.05' />
+          <stop offset='0.771712' stopColor='#F0F2FD' stopOpacity='0.05' />
+          <stop offset='1' stopColor='#EDF6FF' />
+        </linearGradient>
+        <linearGradient
+          id='paint2_linear_12621_2183'
+          x1='449.864'
+          y1='248.996'
+          x2='449.864'
+          y2='650.731'
+          gradientUnits='userSpaceOnUse'
+        >
+          <stop stopColor='#DBEBFF' />
+          <stop offset='0.234014' stopColor='#F0F2FD' stopOpacity='0.05' />
+          <stop offset='0.771712' stopColor='#F0F2FD' stopOpacity='0.05' />
+          <stop offset='1' stopColor='#EDF6FF' />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+};
+
+export default SignUpBackground;
