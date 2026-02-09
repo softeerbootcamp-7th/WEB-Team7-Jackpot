6b4ed1960f0bb2acc110a6bb7c9b93d53b921711
livebylee <livebylee@gmail.com>
2026-02-09 15:13:02 +0900

    [feat] keyword size 2 이상 valid 추가 (#242)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/SearchController.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/SearchController.java
@@ -4,14 +4,17 @@ import com.jackpot.narratix.domain.controller.api.SearchApi;
 import com.jackpot.narratix.domain.controller.response.SearchScrapResponse;
 import com.jackpot.narratix.domain.service.SearchService;
 import com.jackpot.narratix.global.auth.UserId;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/search")
 public class SearchController implements SearchApi {
@@ -22,6 +25,7 @@ public class SearchController implements SearchApi {
     @GetMapping("/scrap")
     public ResponseEntity<SearchScrapResponse> searchScrap(
             @UserId String userId,
+            @Size(min = 2, message = "검색어는 2자 이상이어야 합니다.")
             @RequestParam(required = false) String searchWord,
             @RequestParam(required = false, defaultValue = "10") Integer size,
             @RequestParam(required = false) Long lastQnaId
--- backend/src/main/java/com/jackpot/narratix/domain/controller/api/SearchApi.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/api/SearchApi.java
@@ -10,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.constraints.Size;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -31,7 +32,8 @@ public interface SearchApi {
     @GetMapping
     ResponseEntity<SearchScrapResponse> searchScrap(
             @Parameter(hidden = true) @UserId String userId,
-            @Parameter(description = "검색 키워드") @RequestParam(required = false) String searchWord,
+            @Parameter(description = "검색 키워드")
+            @Size(min = 2, message = "검색어는 2자 이상이어야 합니다.") @RequestParam(required = false) String searchWord,
             @Parameter(description = "페이지 크기") @RequestParam(required = false, defaultValue = "10") Integer size,
             @Parameter(description = "마지막 문항 아이디") @RequestParam(required = false) Long lastQnaId
     );
