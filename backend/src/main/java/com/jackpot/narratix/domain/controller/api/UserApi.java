33650c960e4f48f1fcc52f5d8e4492ed2659eca9
kseysh <kseysh123@naver.com>
2026-02-09 09:55:59 +0900

    [docs] swagger에 SecurityRequirement 추가 (#326)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/api/UserApi.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/api/UserApi.java
@@ -7,11 +7,13 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Tag(name = "사용자", description = "사용자 정보 관련 API")
+@SecurityRequirement(name = "JWT")
 public interface UserApi {
 
     @Operation(summary = "닉네임 조회", description = "로그인한 사용자의 닉네임을 조회합니다.")
