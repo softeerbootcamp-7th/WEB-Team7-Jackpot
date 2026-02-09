949045eb432335a2d06181ad473e1c9166bedaba
윤종근 <doodukly1999@gmail.com>
2026-02-09 11:43:23 +0900

    [feat] 기획의 요구사항에 맞는 파일 유무에 따른 브라우저 확인창 추가
    파일이 있다면 브라우저 확인창을 띄워주기 위해 상태 추가

--- frontend/src/features/upload/components/ContentArea.tsx
+++ frontend/src/features/upload/components/ContentArea.tsx
@@ -1,26 +1,34 @@
+import { useState } from 'react';
+
 import FirstContentAreaHeader from '@/features/upload/components/FirstContentAreaHeader';
 import SecondContentArea from '@/features/upload/components/SecondContentArea';
 import ThirdContentArea from '@/features/upload/components/ThirdContentArea';
 import UploadFileArea from '@/features/upload/components/UploadFileArea';
 import UploadTextArea from '@/features/upload/components/UploadTextArea';
 import type { FirstContentAreaHeaderProps } from '@/features/upload/types/upload';
-
 const ContentArea = ({
   uploadTab,
   setUploadTab,
   step,
   nextStep,
 }: FirstContentAreaHeaderProps) => {
+  const [isContent, setIsContent] = useState<boolean>(false);
   return (
     <div className='flex flex-col gap-4 rounded-2xl border border-gray-100 p-6'>
       {step === '1' && (
         <>
           <FirstContentAreaHeader
+            isContent={isContent}
+            setIsContent={setIsContent}
             uploadTab={uploadTab}
             setUploadTab={setUploadTab}
             nextStep={nextStep}
           />
-          {uploadTab === 'file' ? <UploadFileArea /> : <UploadTextArea />}
+          {uploadTab === 'file' ? (
+            <UploadFileArea setIsContent={setIsContent} />
+          ) : (
+            <UploadTextArea setIsContent={setIsContent} />
+          )}
         </>
       )}
       {step === '2' && (
