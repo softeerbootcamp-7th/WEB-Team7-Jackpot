62faf57202c3d142609a017f885b12e7732ab51c
윤종근 <doodukly1999@gmail.com>
2026-02-10 09:20:18 +0900

    [design] 비회원 랜딩 페이지에서 사용할 아이콘 컴포넌트 추가

---
+++ frontend/src/features/landing/icons/index.ts
@@ -0,0 +1,13 @@
+import CheckIcon from '@/features/landing/icons/CheckIcon';
+import FirstCardIcon from '@/features/landing/icons/FirstCardIcon';
+import RightArrowIcon from '@/features/landing/icons/RightArrowIcon';
+import SecondCardIcon from '@/features/landing/icons/SecondCardIcon';
+import ThirdCardIcon from '@/features/landing/icons/ThirdCardIcon';
+
+export const LandingPageIcon = {
+  CheckIcon: CheckIcon,
+  FirstCardIcon: FirstCardIcon,
+  SecondCardIcon: SecondCardIcon,
+  ThirdCardIcon: ThirdCardIcon,
+  RightArrowIcon: RightArrowIcon,
+};
