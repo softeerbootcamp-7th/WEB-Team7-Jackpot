abc9282eaabd0901ce390e3e252795ca7d4331e3
윤종근 <doodukly1999@gmail.com>
2026-02-10 19:56:20 +0900

    [refactor] 아이콘 경로 변경에 따른 제거

--- frontend/src/shared/icons/index.ts
+++ frontend/src/shared/icons/index.ts
@@ -1,9 +1,7 @@
-import NotificationIcon from '@/shared/icons/NotificationIcon';
 import TitleLogo from '@/shared/icons/TitleLogo';
 import UserAvatarIcon from '@/shared/icons/UserAvatarIcon';
 
 export const CommonIcon = {
-  NotificationIcon: NotificationIcon,
   UserAvatarIcon: UserAvatarIcon,
   TitleLogo: TitleLogo,
 };
