2b52750de071a34f37339cee355138c76052ac21
using6843@gmail.com <using6843@gmail.com>
2026-02-11 17:30:20 +0900

    [feat] 자기소개서 등록하기 컴포넌트

---
+++ frontend/src/features/coverLetter/components/NewCoverLetterContent.tsx
@@ -0,0 +1,7 @@
+import NewCoverLetter from '@/features/coverLetter/components/newCoverLetter/NewCoverLetter';
+
+const NewCoverLetterContent = () => {
+  return <NewCoverLetter />;
+};
+
+export default NewCoverLetterContent;
