98307c6e3322e239e6358d33c52f3cefccda7b45
윤종근 <doodukly1999@gmail.com>
2026-02-11 14:56:53 +0900

    [fix] 닉네임 가져오는 api method 수정

--- frontend/src/shared/api/user.ts
+++ frontend/src/shared/api/user.ts
@@ -3,6 +3,6 @@ import type { UserResponse } from '@/shared/types/userApi';
 
 export const userInformation = {
   getNickname: async (): Promise<UserResponse> => {
-    return await apiClient.post({ endpoint: '/user/nickname' });
+    return await apiClient.get({ endpoint: '/user/nickname' });
   },
 };
