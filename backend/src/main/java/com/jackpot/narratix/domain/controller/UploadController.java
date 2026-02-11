a5b138f46172a87680bb033aad50b5af7f4f51fa
livebylee <livebylee@gmail.com>
2026-02-12 00:51:24 +0900

    [fix] 메서드 수정 (#240)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/UploadController.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/UploadController.java
@@ -3,10 +3,11 @@ package com.jackpot.narratix.domain.controller;
 import com.jackpot.narratix.domain.controller.api.UploadApi;
 import com.jackpot.narratix.domain.controller.request.PresignedUrlRequest;
 import com.jackpot.narratix.domain.controller.response.PresignedUrlResponse;
+import com.jackpot.narratix.domain.service.UploadService;
 import com.jackpot.narratix.global.auth.UserId;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class UploadController implements UploadApi {
     private final UploadService uploadService;
 
     @Override
-    @GetMapping("/presignedurl")
+    @PostMapping("/presignedurl")
     public ResponseEntity<PresignedUrlResponse> createPresignedUrl(
             @UserId String userId,
             @RequestBody PresignedUrlRequest request
