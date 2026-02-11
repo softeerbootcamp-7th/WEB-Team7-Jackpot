8fed70ddd6226a2ed7b7e658d4c4575662777dd4
using6843@gmail.com <using6843@gmail.com>
2026-02-11 17:32:52 +0900

    [feat] TabItem Link로 수정(tab 클릭시 url params 수정)

--- frontend/src/shared/components/TabItem.tsx
+++ frontend/src/shared/components/TabItem.tsx
@@ -1,24 +1,24 @@
+import { Link } from 'react-router';
+
 import type { TabContentType } from '@/shared/types/tab';
 
 interface TabProps<T> {
   currentTab: T;
-  handleTabChange: (tab: T) => void;
   content: TabContentType<T>;
 }
 
-const TabItem = <T extends string>({
-  currentTab,
-  handleTabChange,
-  content,
-}: TabProps<T>) => {
-  const { name, label, icon } = content;
+const TabItem = <T extends string>({ currentTab, content }: TabProps<T>) => {
+  const { name, label, icon, path } = content;
+  const isActive = currentTab === name;
 
   return (
-    <button
-      onClick={() => handleTabChange?.(name)}
-      className={`flex h-13 items-center justify-start rounded-lg py-3 ${currentTab === name ? 'min-w-[180px] bg-purple-50 pr-5 pl-4' : 'min-w-39 px-5'} `}
+    <Link
+      to={path}
+      className={`flex h-13 items-center justify-start rounded-lg py-3 ${
+        isActive ? 'min-w-[180px] bg-purple-50 pr-5 pl-4' : 'min-w-39 px-5'
+      }`}
     >
-      {currentTab === name ? (
+      {isActive ? (
         <div className='inline-flex items-center justify-center gap-1.5'>
           <div className='h-6 w-6'>{icon}</div>
           <div className='text-title-s justify-start font-bold text-purple-600'>
@@ -30,7 +30,7 @@ const TabItem = <T extends string>({
           {label}
         </div>
       )}
-    </button>
+    </Link>
   );
 };
 
