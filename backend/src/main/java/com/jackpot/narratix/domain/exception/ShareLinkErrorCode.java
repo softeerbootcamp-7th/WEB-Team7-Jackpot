b5eff3a9db854c3fb1815261c461b8ee3e290d37
kseysh <kseysh123@naver.com>
2026-02-12 10:24:01 +0900

    [refactor] SHARE_LINK_EXPIRED 상태 코드 변경 (#358)

--- backend/src/main/java/com/jackpot/narratix/domain/exception/ShareLinkErrorCode.java
+++ backend/src/main/java/com/jackpot/narratix/domain/exception/ShareLinkErrorCode.java
@@ -10,7 +10,7 @@ import org.springframework.http.HttpStatus;
 public enum ShareLinkErrorCode implements ErrorCode {
 
     SHARE_LINK_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 첨삭 링크를 찾을 수 없습니다."),
-    SHARE_LINK_EXPIRED(HttpStatus.CONFLICT, "첨삭 링크가 만료되었습니다."),
+    SHARE_LINK_EXPIRED(HttpStatus.GONE, "첨삭 링크가 만료되었습니다."),
     SHARE_LINK_ACCESS_LIMIT_EXCEEDED(HttpStatus.CONFLICT, "첨삭 링크에 접근 가능한 인원 수가 초과되었습니다.")
     ;
 
