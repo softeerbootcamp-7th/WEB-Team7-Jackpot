f03fa18e34d793f9c2d31ff8839a5d56c74ace9a
kseysh <kseysh123@naver.com>
2026-02-14 17:22:31 +0900

    [feat] ReviewCreateRequest에 NotNull 어노테이션 추 (#387)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/request/ReviewCreateRequest.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/request/ReviewCreateRequest.java
@@ -6,7 +6,9 @@ import jakarta.validation.constraints.PositiveOrZero;
 
 public record ReviewCreateRequest(
         @NotNull(message = "version은 필수 입력 항목입니다.") Long version,
+        @NotNull(message = "시작 인덱스는 필수 입력 항목입니다.")
         @PositiveOrZero(message = "startIdx는 0이거나 양수여야 합니다.") Long startIdx,
+        @NotNull(message = "끝 인덱스는 필수 입력 항목입니다.")
         @PositiveOrZero(message = "endIdx는 0이거나 양수여야 합니다.") Long endIdx,
         @NotNull(message = "originText는 필수 입력 항목입니다.") String originText,
         String suggest,
