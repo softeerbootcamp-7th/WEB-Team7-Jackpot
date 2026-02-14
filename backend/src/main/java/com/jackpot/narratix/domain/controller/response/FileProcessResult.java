ee793d9da744193ca9b8bebb93befe8428bccd4a
livebylee <livebylee@gmail.com>
2026-02-15 01:06:42 +0900

    [fix] 불필요한 어노테이션 제거 (#381)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/response/FileProcessResult.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/response/FileProcessResult.java
@@ -2,13 +2,12 @@ package com.jackpot.narratix.domain.controller.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.jackpot.narratix.domain.entity.enums.UploadStatus;
-import jakarta.persistence.Column;
 
 public record FileProcessResult(
         @JsonProperty("status") UploadStatus status,
         @JsonProperty("fileId") String fileId,
         @JsonProperty("fileName") String fileName,
-        @Column(columnDefinition = "TEXT") @JsonProperty("extractedText") String extractedText,
+        @JsonProperty("extractedText") String extractedText,
         @JsonProperty("errorMessage") String errorMessage
 ) {
 }
