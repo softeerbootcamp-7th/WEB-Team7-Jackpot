4be47a807be016e8b3835db4cc5b2059730a3a2f
Sminp <0102262@gmail.com>
2026-02-15 09:01:11 +0900

    [design] 나의 채용공고 페이지에 필요한 아이콘 추가 및 index.ts으로 한번에 관리

---
+++ frontend/src/features/recruit/icons/RecruitEdit.tsx
@@ -0,0 +1,31 @@
+import { type SVGProps } from "react";
+
+interface IProps extends SVGProps<SVGSVGElement> {}
+
+export const RecruitEditIcon = (props: IProps) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="32"
+      height="32"
+      fill="none"
+      viewBox="0 0 32 32"
+      {...props}
+    >
+      <g strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.353">
+        <path
+          fill="#E3E8FC"
+          stroke="#E3E8FC"
+          d="m7.84 20.627-1.176 4.706 4.706-1.176L25 10.527a2.353 2.353 0 0 0 0-3.328l-.202-.202a2.353 2.353 0 0 0-3.327 0z"
+        />
+        <path stroke="#E3E8FC" d="m19.61 8.862 3.529 3.53" />
+        <path stroke="#7371E3" d="M17.258 25.333h9.412" />
+        <path
+          fill="#E3E8FC"
+          stroke="#E3E8FC"
+          d="m7.84 20.627-1.176 4.706 4.706-1.177 11.765-11.764-3.53-3.53z"
+        />
+      </g>
+    </svg>
+  );
+};
