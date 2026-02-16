baa1df7714b24dad9c13513d15966de0228e4e64
kseysh <kseysh123@naver.com>
2026-02-16 20:00:55 +0900

    [chore] 불필요한 어노테이션 제 (#413)

--- backend/src/main/java/com/jackpot/narratix/domain/entity/enums/WebSocketMessageType.java
+++ backend/src/main/java/com/jackpot/narratix/domain/entity/enums/WebSocketMessageType.java
@@ -1,10 +1,5 @@
 package com.jackpot.narratix.domain.entity.enums;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum WebSocketMessageType {
     REVIEW_CREATED,
     REVIEW_UPDATED,
