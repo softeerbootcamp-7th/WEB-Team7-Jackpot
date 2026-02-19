0f1607c4d30780682f4b1d0636fb52c154f3643b
livebylee <livebylee@gmail.com>
2026-02-19 19:17:04 +0900

    [feat] lambdaClient config 추가 (#450)

---
+++ backend/src/main/java/com/jackpot/narratix/global/config/LambdaConfig.java
@@ -0,0 +1,15 @@
+package com.jackpot.narratix.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.services.lambda.LambdaClient;
+
+@Configuration
+public class LambdaConfig {
+
+    @Bean
+    public LambdaClient lambdaClient() {
+        return LambdaClient.builder()
+                .build();
+    }
+}
