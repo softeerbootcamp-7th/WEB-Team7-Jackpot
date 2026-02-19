c1f769569c4bd2b942318156b20fb4858612f598
livebylee <livebylee@gmail.com>
2026-02-19 11:23:52 +0900

    [feat] JobCreateRequest dto 생성(#442)

---
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/request/JobCreateRequest.java
@@ -0,0 +1,21 @@
+package com.jackpot.narratix.domain.controller.request;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record JobCreateRequest(
+        @NotEmpty(message = "최소 하나 이상의 파일 정보가 필요합니다.")
+        @Valid @NotNull List<FileRequest> files
+) {
+
+    public record FileRequest(
+            @NotBlank String presignedUrl,
+            @NotBlank String fileKey
+    ) {
+
+    }
+}
