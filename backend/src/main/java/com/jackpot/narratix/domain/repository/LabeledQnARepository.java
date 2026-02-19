815931e1fb81f266982fb01a3b5e474a4876196a
kseysh <kseysh123@naver.com>
2026-02-19 17:49:35 +0900

    [feat] 업로드 후 자기소개서 조회 레포지토리 로직 추가 (#453)

--- backend/src/main/java/com/jackpot/narratix/domain/repository/LabeledQnARepository.java
+++ backend/src/main/java/com/jackpot/narratix/domain/repository/LabeledQnARepository.java
@@ -2,6 +2,13 @@ package com.jackpot.narratix.domain.repository;
 
 import com.jackpot.narratix.domain.entity.LabeledQnA;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface LabeledQnARepository extends JpaRepository<LabeledQnA, String> {
+import java.util.List;
+
+public interface LabeledQnARepository extends JpaRepository<LabeledQnA, Long> {
+
+    @Query("SELECT lq FROM LabeledQnA lq JOIN FETCH lq.uploadFile uf WHERE uf.uploadJob.id = :uploadJobId")
+    List<LabeledQnA> findAllByUploadJobId(@Param("uploadJobId") String uploadJobId);
 }
