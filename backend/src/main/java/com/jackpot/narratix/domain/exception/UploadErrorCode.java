d940bc942ea6f574d39e17ebe0b4e1c048016c30
livebylee <livebylee@gmail.com>
2026-02-20 18:14:50 +0900

    [feat] 에러 코드 추가 (#450)

--- backend/src/main/java/com/jackpot/narratix/domain/exception/UploadErrorCode.java
+++ backend/src/main/java/com/jackpot/narratix/domain/exception/UploadErrorCode.java
@@ -12,6 +12,7 @@ public enum UploadErrorCode implements ErrorCode {
     INVALID_CONTENT_TYPE_FOR_PDF(HttpStatus.UNSUPPORTED_MEDIA_TYPE, "PDF만 업로드할 수 있습니다."),
     FILE_SIZE_EXCEEDED(HttpStatus.valueOf(413), "파일 업로드 용량이 초과되었습니다."),
     PRESIGNED_URL_GENERATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "S3 업로드 URL 생성에 실패했습니다."),
+    LAMBDA_CALL_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "lambda 호출을 실패했습니다. "),
     EMPTY_FILE_LIST(HttpStatus.BAD_REQUEST, "파일 리스트가 비었습니다."),
     TOO_MANY_FILES(HttpStatus.BAD_REQUEST, "파일 개수가 너무 많습니다."),
     INVALID_FILE_KEY(HttpStatus.BAD_REQUEST, "유효하지 않는 파일 키 형식입니다."),
