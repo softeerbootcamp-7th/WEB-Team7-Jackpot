a335ea5b462d356c2f6931c14f6c2dcb30db2b0e
livebylee <livebylee@gmail.com>
2026-02-20 13:39:19 +0900

    [fix] 직무명, 기업명 notblank로 제약사항 변경 (#476)

--- backend/src/main/java/com/jackpot/narratix/domain/controller/request/CoverLetterAndQnAEditRequest.java
+++ backend/src/main/java/com/jackpot/narratix/domain/controller/request/CoverLetterAndQnAEditRequest.java
@@ -4,6 +4,7 @@ import com.jackpot.narratix.domain.entity.enums.ApplyHalfType;
 import com.jackpot.narratix.domain.entity.enums.QuestionCategoryType;
 import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
@@ -16,10 +17,10 @@ public record CoverLetterAndQnAEditRequest(
 
     public record CoverLetterEditRequest(
             @NotNull(message = "자기소개서 아이디는 필수 입력 항목입니다.") Long coverLetterId,
-            @NotNull(message = "기업명은 필수 입력 항목입니다.") String companyName,
+            @NotBlank(message = "기업명은 필수 입력 항목입니다.") String companyName,
             @NotNull(message = "채용년도는 필수 입력 항목입니다.") Integer applyYear,
             @NotNull(message = "채용분기는 필수 입력 항목입니다.") ApplyHalfType applyHalf,
-            @NotNull(message = "직무명은 필수 입력 항목입니다.") String jobPosition,
+            @NotBlank(message = "직무명은 필수 입력 항목입니다.") String jobPosition,
             @Nullable LocalDate deadline
     ) {
     }
