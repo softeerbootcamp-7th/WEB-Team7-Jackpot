e33ca343b00ff9fa98f113180704f857d030f3db
livebylee <livebylee@gmail.com>
2026-02-21 16:45:15 +0900

    [feat] sendLabelingCompeleteNotification 메서드 정의  (#451)

--- backend/src/main/java/com/jackpot/narratix/domain/service/NotificationService.java
+++ backend/src/main/java/com/jackpot/narratix/domain/service/NotificationService.java
@@ -1,16 +1,17 @@
 package com.jackpot.narratix.domain.service;
 
+import com.jackpot.narratix.domain.controller.response.NotificationsPaginationResponse;
+import com.jackpot.narratix.domain.controller.response.UnreadNotificationCountResponse;
+import com.jackpot.narratix.domain.entity.Notification;
 import com.jackpot.narratix.domain.entity.User;
 import com.jackpot.narratix.domain.entity.enums.NotificationType;
 import com.jackpot.narratix.domain.entity.notification_meta.FeedbackNotificationMeta;
+import com.jackpot.narratix.domain.entity.notification_meta.LabelingNotificationMeta;
 import com.jackpot.narratix.domain.event.NotificationSendEvent;
+import com.jackpot.narratix.domain.repository.NotificationRepository;
 import com.jackpot.narratix.domain.repository.UserRepository;
 import com.jackpot.narratix.domain.service.dto.NotificationSendRequest;
 import com.jackpot.narratix.domain.service.dto.NotificationSendResponse;
-import com.jackpot.narratix.domain.controller.response.UnreadNotificationCountResponse;
-import com.jackpot.narratix.domain.controller.response.NotificationsPaginationResponse;
-import com.jackpot.narratix.domain.entity.Notification;
-import com.jackpot.narratix.domain.repository.NotificationRepository;
 import com.jackpot.narratix.global.exception.BaseException;
 import com.jackpot.narratix.global.exception.GlobalErrorCode;
 import lombok.RequiredArgsConstructor;
@@ -29,6 +30,8 @@ public class NotificationService {
     private final NotificationRepository notificationRepository;
     private final ApplicationEventPublisher eventPublisher;
 
+    private static final String LABELING_NOTIFICATION_TITLE = "총 {}개의 자기소개서를 분석했어요";
+
     @Transactional(readOnly = true)
     public NotificationsPaginationResponse getNotificationsByUserId(
             String userId, Optional<Long> lastNotificationId, Integer size
@@ -44,7 +47,7 @@ public class NotificationService {
     public void markNotificationAsRead(String userId, Long notificationId) {
         Notification notification = notificationRepository.findByIdOrElseThrow(notificationId);
 
-        if(!notification.isOwner(userId)) throw new BaseException(GlobalErrorCode.FORBIDDEN);
+        if (!notification.isOwner(userId)) throw new BaseException(GlobalErrorCode.FORBIDDEN);
 
         notification.read();
     }
@@ -61,7 +64,7 @@ public class NotificationService {
     }
 
     @Transactional
-    public void sendFeedbackNotificationToWriter(String reviewerId, String writerId, String notificationTitle, Long qnAId, String originText){
+    public void sendFeedbackNotificationToWriter(String reviewerId, String writerId, String notificationTitle, Long qnAId, String originText) {
         User reviewer = userRepository.findByIdOrElseThrow(reviewerId);
 
         FeedbackNotificationMeta feedbackNotificationMeta = FeedbackNotificationMeta.of(
@@ -80,4 +83,25 @@ public class NotificationService {
 
         eventPublisher.publishEvent(new NotificationSendEvent(writerId, NotificationSendResponse.of(notification)));
     }
+
+    @Transactional
+    public void sendLabelingCompleteNotification(String userId, String jobId, Long successCount, Long failCount) {
+
+        LabelingNotificationMeta labelingNotificationMeta = LabelingNotificationMeta.of(
+                jobId, successCount, failCount
+        );
+
+        NotificationSendRequest request = NotificationSendRequest.builder()
+                .type(NotificationType.LABELING_COMPLETE)
+                .meta(labelingNotificationMeta)
+                .title(LABELING_NOTIFICATION_TITLE.formatted(successCount + failCount))
+                .content(null)
+                .build();
+
+        Notification notification = request.toEntity(userId);
+        notificationRepository.save(notification);
+
+        eventPublisher.publishEvent(new NotificationSendEvent(userId, NotificationSendResponse.of(notification)));
+    }
+
 }
