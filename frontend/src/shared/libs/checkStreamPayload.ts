a758cee344ba9521b8dcce2d7df2b3524d76a742
윤종근 <doodukly1999@gmail.com>
2026-02-21 21:37:59 +0900

    [fix] 알림 payload 변경에 따른 타입 가드 수정

--- frontend/src/shared/libs/checkStreamPayload.ts
+++ frontend/src/shared/libs/checkStreamPayload.ts
@@ -1,11 +1,15 @@
 import type { NotificationSSEPayload } from '@/shared/types/sse';
 
 // 스트림으로 들어온 데이터가 알림 페이로드인지 확인하는 함수 (타입 가드)
-export const isNotificationPayload = (data: unknown): data is NotificationSSEPayload => {
+export const isNotificationPayload = (
+  data: unknown,
+): data is NotificationSSEPayload => {
+  console.log(data);
   return (
     typeof data === 'object' &&
     data !== null &&
-    'notification' in data &&
-    'unreadNotificationCount' in data
+    'type' in data &&
+    'isRead' in data &&
+    'id' in data
   );
 };
