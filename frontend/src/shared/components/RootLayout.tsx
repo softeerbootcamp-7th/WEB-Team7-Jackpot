0b5f1520ae55f4979ed93f538d23595430a9ed19
윤종근 <doodukly1999@gmail.com>
2026-02-24 09:25:52 +0900

    [design] 스크롤 바 영역 RootLayout에 적용

--- frontend/src/shared/components/RootLayout.tsx
+++ frontend/src/shared/components/RootLayout.tsx
@@ -1,10 +1,22 @@
+import { useEffect } from 'react';
+
 import { Outlet } from 'react-router';
 
 import PageGlobalHeader from '@/shared/components/PageGlobalHeader';
 import { useSSE } from '@/shared/hooks/useSSE';
 
 const RootLayout = () => {
-  useSSE()
+  useSSE();
+
+  useEffect(() => {
+    // RootLayout이 렌더링되는 서비스 페이지 진입 시 html에 클래스 추가
+    document.documentElement.classList.add('stable-layout');
+
+    // 컴포넌트가 사라질 때(랜딩으로 나갈 때) 클래스 제거
+    return () => {
+      document.documentElement.classList.remove('stable-layout');
+    };
+  }, []);
   return (
     <div>
       <PageGlobalHeader />
